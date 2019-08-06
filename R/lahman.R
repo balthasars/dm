65bcbf85cc36806f545a602ca5ed9c048af4e019
balthasar <balthasar@cynkra.com>
2019-08-06 15:25:35 +0200

    work on function

--- R/lahman.R
+++ R/lahman.R
@@ -1,9 +1,7 @@
-#' Creates a test [`dm`] object from \pkg{nycflights13}
+#' Creates a test [`dm`] object from \pkg{Lahman}
 #'
-#' @description Creates an exemplary [`dm`] object from the tables in \pkg{nycflights13}
-#' along with key relations (two foreign key relations are violated, but we accept this,
-#' since this should only be seen as a quick way of getting a [`dm`] with known tables to
-#' play around with).
+#' @description Creates an exemplary [`dm`] object from the tables in \pkg{Lahman}
+#' along with key relations.
 #'
 #' @param cycle Boolean. If `FALSE` (default), only one foreign key relation
 #'   (from `flights$origin` to `airports$faa`) between `flights` and `airports` is
@@ -44,3 +42,81 @@ cdm_nycflights13 <- function(cycle = FALSE, color = TRUE) h(~ {
       dm
     }
   })
+
+cdm_lahman <- function(color = TRUE){
+
+}
+
+library(dplyr)
+library(dm)
+
+# dm::cdm_learn_from_db(dbplyr::copy_lahman(dplyr::src_postgres()))
+
+# lahman_dm_raw <- dplyr::src_df("Lahman") %>%
+#   dm::as_dm()
+# cannot remove tables from dm object
+# thus verbose, as dbplyr:::lahman_tables
+# also lists too many
+
+
+library(Lahman)
+lahman_dm_raw <- as_dm(
+  list(
+    "AllstarFull"
+    "Appearances"
+    "AwardsManagers"
+    "AwardsPlayers"
+    "AwardsShareManagers"
+    "AwardsSharePlayers"
+    "Batting"
+    "BattingPost"
+    "CollegePlaying"
+    "Fielding"
+    "FieldingOF"
+    "FieldingPost"
+    "HallOfFame"
+    # LahmanData, # meta table
+    "Managers",
+    "ManagersHalf",
+    # Master, # deprecated
+    "Parks",
+    "People",
+    "Pitching",
+    "PitchingPost",
+    "Salaries",
+    "Schools",
+    "SeriesPost",
+    "Teams",
+    "TeamsFranchises",
+    "TeamsHalf"
+  )
+)
+
+# which packages are okay to use for building
+# functions? get dependencies:
+# itdepends::dep_usage_pkg("dm") %>% distinct(pkg)
+
+
+lahman_dm_raw %>%
+  cdm_enum_fk_candidates(AwardsManagers, Managers)
+
+lahman_dm_raw %>%
+  map(cdm_enum_fk_candidates)
+
+map_named <- function(x, ...) map(x, ...) %>%
+  set_names(x)
+
+lahman_dm_raw %>%
+  cdm_get_tables() %>%
+  attr("names") %>%
+  map_named(cdm_enum_pk_candidates, dm = lahman_dm_raw) %>%
+  enframe() %>%
+  unnest() %>%
+  filter(candidate)
+
+
+lahman_dm_raw %>%
+  cdm_add_pk(People, playerID) %>%
+  cdm_add_pk()
+
+# People: players
